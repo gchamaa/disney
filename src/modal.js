8623949ae8025dd3fcda37e738a530a65e01a4c7
Gilbert Chamaa <gchamaa@yahoo.com>
2025-01-07 06:28:09 -0800

    refactor: refactored modal to get rid of FOUC

--- src/modal.js
+++ src/modal.js
@@ -1,24 +1,56 @@
-const modal = document.getElementById('modal');
-// const modalHeader = document.querySelector('#modal .modalHeader');
-// const modalBody = document.querySelector('#modal .modalBody');
-const closeModalButton = document.getElementById('closeModal');
-const overlay = document.getElementById('overlay');
 
-overlay.addEventListener('click', closeModal);
-closeModalButton.addEventListener('click', closeModal);
+const modalNode = document.createElement("div");
+modalNode.classList.add("modal");
+modalNode.setAttribute("id", "modal");
+document.querySelector("body").appendChild(modalNode);
+
+const modalHeaderNode = document.createElement("div");
+modalHeaderNode.classList.add("modalHeader");
+
+const modalBodyNode = document.createElement("div");
+modalBodyNode.classList.add("modalBody");
+modalBodyNode.textContent = "This is the content of the modal.";
+
+const modalFooterNode = document.createElement("div");
+modalFooterNode.classList.add("modalFooter");
+
+const footerPrimaryBtnNode = document.createElement("button");
+footerPrimaryBtnNode.classList.add("btnPrimary");
+footerPrimaryBtnNode.textContent="Watch";
+
+const footerSecondaryBtnNode = document.createElement("button");
+footerSecondaryBtnNode.classList.add("btnSecondary");
+footerSecondaryBtnNode.setAttribute("id", "closeModal");
+footerSecondaryBtnNode.addEventListener('click', closeModal);
+footerSecondaryBtnNode.textContent = "Close";
+
+modalFooterNode.appendChild(footerPrimaryBtnNode);
+modalFooterNode.appendChild(footerSecondaryBtnNode);
+
+modalNode.appendChild(modalHeaderNode);
+modalNode.appendChild(modalBodyNode);
+modalNode.appendChild(modalFooterNode);
+document.body.appendChild(modalNode);
+
+
+const overlayNode = document.createElement("div");
+overlayNode.classList.add("overlay");
+overlayNode.setAttribute("id", "overlay");
+overlayNode.addEventListener('click', closeModal);
+document.body.appendChild(overlayNode);
 
 let trigger = null;
 
 function openModal(event) {
-    modal.classList.add('active');
-    overlay.classList.add('active');
-    closeModalButton.focus();
+    modalNode.classList.add('active');
+    overlayNode.classList.add('active');
+    footerSecondaryBtnNode.focus();
     trigger = event.target;
 }
 
 function closeModal() {  
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
+    modalNode.classList.remove('active');
+    overlayNode.classList.remove('active');
     trigger.focus();
 }
 
